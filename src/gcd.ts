1e9c1b200efae2f392f1b489ec0d42d53dff2143
JimGitFE <rymsjfjrys@gmail.com>
2024-03-11 18:57:47 -0300

    refactor/es6

--- src/gcd.ts
+++ src/gcd.ts
@@ -1,16 +1,8 @@
 const gcd = ({r0, r1}: {r0: number, r1: number}) => {
-    console.log('gcd');
-    let modulus = 1;
-    let rl = r0
-    let rn = r1
-    while (modulus !== 0) {
-        modulus = rl % rn
-        rl = rn
-        rn = modulus
-        if (modulus === 0) {
-            return rl
-        }
+    while (r1 !== 0) {
+        [r0, r1] = [r1, r0 % r1]
     }
+    return r0
 }
 
 console.log(gcd({r0: 973, r1: 301}))
